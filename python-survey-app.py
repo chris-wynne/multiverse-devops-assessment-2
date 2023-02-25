ce5a95e6ad5ed3bd8755edec9b74cd38280abbc0
Chris Wynne <Chris.Wynne@arup.com>
2023-02-25 21:25:12 +0000

    t2 remove duplicate entries

--- python-survey-app.py
+++ python-survey-app.py
@@ -1,5 +1,16 @@
+import numpy as np
 from scripts.import_data import get_input
+from scripts.clean_data import identify_duplicate_values, remove_duplicates
+
+
 
 fpath = r"data/results.csv"
 
-dataset = get_input(fpath)
+dataset = get_input(fpath) #import dataset
+data_array = dataset
+index_col_position = len(dataset[0])-1 #set position of index column (last column)
+
+duplicate_index_list = identify_duplicate_values(dataset, 0, index_col_position)
+clean_array = remove_duplicates(dataset, index_col_position, duplicate_index_list)
+
+print(clean_array)
