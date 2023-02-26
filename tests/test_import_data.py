6b1ee374b0fcebfb5859b452a885f6178c57cfc4
Chris Wynne <Chris.Wynne@arup.com>
2023-02-26 20:11:27 +0000

    temp csv file fixture

--- tests/test_import_data.py
+++ tests/test_import_data.py
@@ -1,4 +1,5 @@
 import pytest
+import csv
 import numpy as np
 from scripts.import_data import get_input, add_index
 from pathlib import Path
@@ -8,6 +9,33 @@ output = get_input(filename)
 column_names = output[0]
 expected_col_len = 6
 expected_names = ["user_id","first_name","last_name","answer_1","answer_2","answer_3"]
+
+#test values for temp csv
+test_columns = ["A", "B", "C"]
+test_rows = [
+        ["a1", "b1", "c1"],
+        ["a2", "b2", "c2"],
+        ["a3", "b3", "c3"],
+        ["", "", ""],
+        ["a4", "b4", "c4"],
+        ["a5", "b5", "c5"],
+        ["a6", "b6", "c6"],
+        ["a7", "b7", "c7"],
+        ["", "", ""],
+        ["a8", "b8", "c8"],
+        ["a9", "b9", "c9"],
+        ["a10", "b10", "c10"]
+    ]
+
+@pytest.fixture(scope='session')
+def file_name(tmpdir_data):
+    file = tmpdir_data.mktemp('data'.join('csvtest.csv'))
+    with open(file, 'a', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(test_columns)
+        for row in test_rows:
+            csv_writer.writerow(row)
+    return file
 
 def test_file_exists():
     file_exists = Path(filename).is_file()
