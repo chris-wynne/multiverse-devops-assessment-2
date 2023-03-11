77137234eacb1a25625cc45465fcd85530f14305
Chris Wynne <Chris.Wynne@arup.com>
2023-03-11 15:02:27 +0000

    added new test column and additonal comments

--- tests/test_clean_data.py
+++ tests/test_clean_data.py
@@ -2,8 +2,7 @@ import pytest
 import numpy as np
 from scripts.clean_data import identify_duplicate_values, remove_duplicates, find_column_position
 
-test_array = np.array([["index", "test_id"], [1, 1], [2, 1], [3, 2], [4, 2], [5, 3], [6, 4]])
-test_dup_indexes = [2,4]
+test_array = np.array([["index", "test_id", "test_name"], [1, 1, "john"], [2, 1, "john"], [3, 2, "paul"], [4, 2, "paul"], [5, 3, "george"], [6, 4, "ringo"]])
 
 def test_identify_duplicate_values():
     duplicate_indexes = identify_duplicate_values(test_array, "test_id")
@@ -12,6 +11,11 @@ def test_identify_duplicate_values():
 def test_remove_duplicates():
     duplicate_indexes = identify_duplicate_values(test_array, "test_id")
     clean_test_array = remove_duplicates(test_array, duplicate_indexes)
+    
+    #expected length of test_array
+    assert len(test_array) == 7
+    
+    #expected length of clean test_array
     assert len(clean_test_array) == 5
 
 def test_find_column_position():
