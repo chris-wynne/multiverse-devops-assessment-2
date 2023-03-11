30d53c38f3e1300f6f02ed4ddd7cfa410232b111
Chris Wynne <Chris.Wynne@arup.com>
2023-03-11 12:56:35 +0000

    csv fixture and test remove blanks

--- tests/test_import_data.py
+++ tests/test_import_data.py
@@ -1,6 +1,7 @@
 import pytest
 import csv
 import numpy as np
+from numpy import genfromtxt
 from scripts.import_data import get_input, add_index
 from pathlib import Path
 
@@ -10,9 +11,9 @@ column_names = output[0]
 expected_col_len = 6
 expected_names = ["user_id","first_name","last_name","answer_1","answer_2","answer_3"]
 
-#test values for temp csv
-test_columns = ["A", "B", "C"]
-test_rows = [
+#test array data for writing to a temporary csv
+test_data = [
+        ["A", "B", "C"],
         ["a1", "b1", "c1"],
         ["a2", "b2", "c2"],
         ["a3", "b3", "c3"],
@@ -27,15 +28,15 @@ test_rows = [
         ["a10", "b10", "c10"]
     ]
 
-@pytest.fixture(scope='session')
-def file_name(tmpdir_data):
-    file = tmpdir_data.mktemp('data'.join('csvtest.csv'))
-    with open(file, 'a', newline='') as csvfile:
+@pytest.fixture(scope="module")
+def get_csv_data(tmp_path_factory):
+    # Create a temporary file in the test directory and writes the sample CSV data (test_data)
+    test_file = tmp_path_factory.mktemp("data").joinpath("test.csv")
+    with test_file.open("w", newline="") as csvfile:
         csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(test_columns)
-        for row in test_rows:
+        for row in test_data:
             csv_writer.writerow(row)
-    return file
+    return test_file
 
 def test_file_exists():
     file_exists = Path(filename).is_file()
@@ -56,4 +57,12 @@ def test_input_column_names():
 
 def test_array_contains_index_col():
     assert "index" in column_names
-    pass
+
+def test_remove_blank_rows(get_csv_data):
+    data_with_blanks = genfromtxt(get_csv_data, delimiter=',')
+    data_without_blanks = get_input(get_csv_data)
+    
+    #checks all 13 rows exist in temporary test csv
+    assert len(data_with_blanks) == 13
+    #checks 2 blank rows have been removed from imported temporary data set
+    assert len(data_without_blanks) == 11
