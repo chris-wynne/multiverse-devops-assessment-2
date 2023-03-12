462022b46f6285f4609b0f74c8d2b6284738f121
Chris Wynne <Chris.Wynne@arup.com>
2023-03-12 14:17:11 +0000

    refactore conftest to remove temp files

--- tests/test_import_data.py
+++ tests/test_import_data.py
@@ -1,6 +1,8 @@
 import pytest
 import csv
 import numpy as np
+import os
+import shutil
 from numpy import genfromtxt
 from pathlib import Path
 from scripts.import_data import get_input, save_output
@@ -34,12 +36,10 @@ def test_input_column_names():
     for col in col_range:
         assert column_names[col] == expected_names[col]
 
-@pytest.fixture(scope="session")
 def test_array_contains_index_col(get_test_csv_data):
     data_without_blanks = get_input(get_test_csv_data)
     assert "index" in data_without_blanks[0]
 
-@pytest.fixture(scope="session")
 def test_remove_blank_rows(get_test_csv_data):
     data_with_blanks = genfromtxt(get_test_csv_data, delimiter=',', dtype='<U13')
     data_without_blanks = get_input(get_test_csv_data)
@@ -48,11 +48,7 @@ def test_remove_blank_rows(get_test_csv_data):
     assert len(data_with_blanks) == 9
     #checks 2 blank rows have been removed from imported temporary data set
     assert len(data_without_blanks) == 7
-
-@pytest.fixture(scope="session")
-def test_save_output(tmp_path_factory):
-    output_test_file = tmp_path_factory.mktemp("data").joinpath("output_test.csv")
-    save_output(expected_data, output_test_file)
-    output_file_exists = Path(output_test_file).is_file()
     
+def test_save_output(gen_save_output_file):
+    output_file_exists = Path(gen_save_output_file).is_file()
     assert output_file_exists == True
